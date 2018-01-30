d36ef8b9809f3bc95cd396233c747cc24fd88692
Naftaly Avadiaev <naftaly.avadiaev@gmail.com>
2018-01-29 18:36:56 -0800

    Buffer.h was added

---
+++ Buffer.h
@@ -0,0 +1,28 @@
+#ifndef _BUFFER_H_
+#define _BUFFER_H_
+
+#include <vector>
+#include <list>
+#include <string>
+#include "Function.h"
+
+using namespace std;
+
+class Buffer {
+    private:
+        vector<string> header;
+        vector<string> code;
+        unsigned int nextLine;
+        vector<Function> implemented;
+        vector<Function> unimplemented;
+
+	public:
+		Buffer() {}
+        ~Buffer() {}
+		void emit(string &instruction) {}
+		void backPatch(list<unsigned int> &lines, string &address);
+		unsigned int nextQuad();
+	
+};
+
+#endif
