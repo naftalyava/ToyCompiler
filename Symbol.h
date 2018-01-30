4de9e716b965dc1000aa82b6ac8ae007401864e8
Naftaly Avadiaev <naftaly.avadiaev@gmail.com>
2018-01-29 18:10:49 -0800

    implement in h file

--- Symbol.h
+++ Symbol.h
@@ -5,8 +5,6 @@
 
 using namespace std;
 
-class SymbolNotFound: public exception {};
-
 class Symbol {
 	private:
 		string m_name;
@@ -23,6 +21,46 @@ class Symbol {
         void setSize(unsigned int size);
         void setOffset(unsigned int offset);
 };
+
+
+Symbol::Symbol(string name, unsigned int size, unsigned int offset) : m_name(name), m_size(size), m_offset(offset) {}
+
+Symbol::~Symbol() {}
+
+bool Symbol::operator==(const Symbol &symbol) 
+{
+	return (this->m_name == symbol.m_name);
+}
+
+string Symbol::getName() const 
+{
+	return m_name;
+}
+
+unsigned int Symbol::getSize() const 
+{
+	return m_size;
+}
+
+unsigned int Symbol::getOffset() const 
+{
+	return m_offset;
+}
+
+void Symbol::setName(string name) 
+{
+	m_name = name;
+}
+
+void Symbol::setSize(unsigned int size) 
+{
+	m_size = size;
+}
+
+void Symbol::setOffset(unsigned int offset) 
+{
+	m_offset = offset;
+}
 
 
 #endif
--- SymbolTable.h
+++ SymbolTable.h
@@ -6,9 +6,10 @@
 #include <vector>
 #include <algorithm>
 
-using namespace std;
-
 class SymbolNotFound: public exception {};
+class SymbolAlreadyExists: public exception {};
+
+using namespace std;
 
 class SymbolTable {
     private:
@@ -21,7 +22,51 @@ class SymbolTable {
         void startBlock();
         void endBlock();
         void addSymbol(string name, unsigned int size);
-        Symbol& findSymbol(string name) const;
+        Symbol& findSymbol(string name);
 }; 
+
+
+SymbolTable::SymbolTable() : level(1), offset(0) 
+{
+	symbols.resize(level);
+}
+
+SymbolTable::~SymbolTable()
+{
+
+}
+
+void SymbolTable::startBlock() 
+{
+	level++;
+	symbols.resize(level);
+}
+
+void SymbolTable::endBlock() 
+{
+	level--;
+	symbols.pop_back();
+}
+
+void SymbolTable::addSymbol(string name, unsigned int size) 
+{
+	symbols[level-1].push_back(Symbol(name, size, offset));
+	//fix offset
+	if (size == 1)
+		offset += 2;
+	else
+		offset += size;
+}
+
+Symbol& SymbolTable::findSymbol(string name) 
+{
+	for(unsigned int i = level - 1; i >= 0; i--){
+		auto r = std::find(symbols[i].begin(), symbols[i].end(), Symbol(name, 0, 0));
+		if (r != end(symbols[i]))
+			return *r;
+	}
+	throw SymbolNotFound();
+} 
+
 
 #endif
