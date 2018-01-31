2e176cc6734ca5ebd4cdcf3926269df1ffa294c1
mohammad1603 <mohammad.abomokh@arm.com>
2018-01-31 18:37:09 +0200

    add register manager

---
+++ RegisterManager.cpp
@@ -0,0 +1,26 @@
+#include "RegisterManager.h"
+
+RegisterManager::RegisterManager() {
+	counter = 0;
+}
+
+RegisterManager::~RegisterManager() {}
+
+int RegisterManager::getRegister(){ //get next register
+
+if(counter == 1023 ){
+	//operational error!
+}
+
+return counter++;
+}
+
+
+int RegisterManager::getRegistersCount(){
+	return counter;
+}
+
+
+void RegisterManager::setRegistersCount(int toSet){
+	counter = toSet;
+}
---
+++ RegisterManager.h
@@ -0,0 +1,17 @@
+#ifndef REGISTERS_MANAGER_H_
+#define REGISTERS_MANAGER_H_
+#include "definitions.h"
+
+
+class RegisterManager {
+int counter;
+
+public:
+	RegisterManager();
+	virtual ~RegisterManager();
+	int getRegister(); //get next available register
+	int getRegistersCount();
+	void setRegistersCount(int toSet);
+};
+
+#endif // REGISTERS_MANAGER_H_
